7eb4eb28c35b8ccc9ad3559124877fee7c155c6d
7iomka <7iomka@gmail.com>
2022-11-27 18:39:39 +0000

    Repro bug use-toggler.ts

--- apps/store/src/shared/use-toggler.ts
+++ apps/store/src/shared/use-toggler.ts
@@ -1,28 +1,14 @@
-import { useUnit, useStore, useEvent } from 'effector-react/scope';
+import { useUnit } from 'effector-react/scope';
 
-// UNCOMMENT THIS version TO SEE BUG (https://t.me/effector_ru/280689)
-// export const useToggler = (togglerInstance: any) => {
-//   const { $isOpen, ...togglerEvents } = togglerInstance;
-//   const isOpen = useUnit($isOpen);
-//   const events = useUnit({ ...togglerEvents });
-
-//   return {
-//     isOpen,
-//     ...events
-//   };
-// };
-
+// THis version product bug described here:
+// https://t.me/effector_ru/280689
 export const useToggler = (togglerInstance: any) => {
   const { $isOpen, ...togglerEvents } = togglerInstance;
-  const isOpen = useStore($isOpen);
-  const open = useEvent(togglerEvents.open);
-  const close = useEvent(togglerEvents.close);
-  const toggle = useEvent(togglerEvents.toggle);
+  const isOpen = useUnit($isOpen);
+  const events = useUnit({ ...togglerEvents });
 
   return {
     isOpen,
-    open,
-    close,
-    toggle,
+    ...events,
   };
 };
