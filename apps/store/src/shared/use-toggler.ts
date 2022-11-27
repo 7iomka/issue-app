c37c68bec87d863f82809d0d8a5662b1f8ea6e78
7iomka <7iomka@gmail.com>
2022-11-27 18:10:05 +0000

    UseUnit replace to useStore/useEvent to fix bug with batching issue

--- apps/store/src/shared/use-toggler.ts
+++ apps/store/src/shared/use-toggler.ts
@@ -1,12 +1,28 @@
-import { useUnit } from 'effector-react';
+import { useUnit, useStore, useEvent } from 'effector-react/scope';
+
+// UNCOMMENT THIS version TO SEE BUG (https://t.me/effector_ru/280689)
+// export const useToggler = (togglerInstance: any) => {
+//   const { $isOpen, ...togglerEvents } = togglerInstance;
+//   const isOpen = useUnit($isOpen);
+//   const events = useUnit({ ...togglerEvents });
+
+//   return {
+//     isOpen,
+//     ...events
+//   };
+// };
 
 export const useToggler = (togglerInstance: any) => {
   const { $isOpen, ...togglerEvents } = togglerInstance;
-  const isOpen = useUnit($isOpen);
-  const events = useUnit({ ...togglerEvents });
+  const isOpen = useStore($isOpen);
+  const open = useEvent(togglerEvents.open);
+  const close = useEvent(togglerEvents.close);
+  const toggle = useEvent(togglerEvents.toggle);
 
   return {
     isOpen,
-    ...events,
+    open,
+    close,
+    toggle,
   };
 };
